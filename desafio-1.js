56fb60482c1e26a2c02ce2ca04e0e5df78f4ab55
ClaudinhoDS <claudio_fernandes_11@hotmail.com>
2023-01-11 21:22:27 -0300

    meu primeiro commit

---
+++ desafio-1.js
@@ -0,0 +1,13 @@
+const clientes = [
+    {nome:"Rafael", email: "rafael@gmail.com", status: true},
+    
+    { nome: "Fernanda", email: "fernanda@gmail.com", status: true },
+
+    { nome: "João", email: "joao@gmail.com", status: false },
+
+    { nome: "Maria", email: "maria@gmail.com", status: false },
+
+    { nome: "Ana", email: "ana@gmail.com", status: true },
+];
+    
+    
